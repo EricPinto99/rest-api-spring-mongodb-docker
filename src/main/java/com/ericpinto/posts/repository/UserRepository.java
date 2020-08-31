26115a2d34545e6e1bd8c29225df378639a60fd1
Éric Pinto <ericgrandopinto@gmail.com>
2020-08-31 19:21:38 -0300

    added User repository

---
+++ src/main/java/com/ericpinto/posts/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ericpinto.posts.repository;
+
+import com.ericpinto.posts.domain.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
