2a138c52002f92ede46529120e8c9df46de08e5e
Éric Pinto <ericgrandopinto@gmail.com>
2020-09-01 17:21:33 -0300

    added user insertion

--- src/main/java/com/ericpinto/posts/controllers/UserController.java
+++ src/main/java/com/ericpinto/posts/controllers/UserController.java
@@ -5,11 +5,10 @@ import com.ericpinto.posts.domain.model.dto.UserDTO;
 import com.ericpinto.posts.domain.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +30,14 @@ public class UserController {
     public ResponseEntity<User> findById(@PathVariable String id){
         User obj = userService.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = userService.fromDTO(objDto);
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 
 }
--- src/main/java/com/ericpinto/posts/domain/services/UserService.java
+++ src/main/java/com/ericpinto/posts/domain/services/UserService.java
@@ -1,13 +1,13 @@
 package com.ericpinto.posts.domain.services;
 
 import com.ericpinto.posts.domain.model.User;
+import com.ericpinto.posts.domain.model.dto.UserDTO;
 import com.ericpinto.posts.domain.services.exception.ObjectNotFoundException;
 import com.ericpinto.posts.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,5 +23,12 @@ public class UserService {
         return userRepository.findById(id).orElseThrow(()-> new ObjectNotFoundException("User não encontrado"));
     }
 
+    public User insert(User obj){
+        return userRepository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(),objDto.getName(), objDto.getEmail());
+    }
 
 }
