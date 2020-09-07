7a6a457292db32698d4c991d26ac360fb7fb4f1e
Éric Pinto <ericgrandopinto@gmail.com>
2020-09-07 15:48:57 -0300

    Referencing user posts

--- src/main/java/com/ericpinto/posts/config/Instantiation.java
+++ src/main/java/com/ericpinto/posts/config/Instantiation.java
@@ -24,21 +24,24 @@ public class Instantiation implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 
         userRepository.deleteAll();
         postRepository.deleteAll();
 
         User eric = new User(null, "Éric Pinto", "eric@gmail.com");
-        User regis = new User(null, "Régis Pinto", "regis@gmail.com");
-        User melania = new User(null, "Melânia", "mel@gmail.com");
+        User regis = new User(null, "Régis S Pinto", "regispinto@gmail.com");
+        User mel = new User(null, "Melania Grando", "mel123@gmail.com");
 
-        userRepository.insert(Arrays.asList(eric, regis, melania));
+        userRepository.saveAll(Arrays.asList(eric, regis, mel));
 
-        Post post1 = new Post(null, simpleDateFormat.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP abraços!", new AuthorDTO(eric));
-        Post post2 = new Post(null, simpleDateFormat.parse("23/03/2018"), "Bom dia", "Acordei feliz", new AuthorDTO(eric));
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(eric));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(eric));
 
-        postRepository.insert(Arrays.asList(post1, post2));
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        eric.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(eric);
     }
 }
