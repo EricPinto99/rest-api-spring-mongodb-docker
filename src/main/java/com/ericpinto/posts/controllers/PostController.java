f0a5662dfec3167b3abbec88559a8c307681f218
Éric Pinto <ericgrandopinto@gmail.com>
2020-09-08 21:06:49 -0300

    getting all posts

--- src/main/java/com/ericpinto/posts/controllers/PostController.java
+++ src/main/java/com/ericpinto/posts/controllers/PostController.java
@@ -1,21 +1,32 @@
 package com.ericpinto.posts.controllers;
 
 import com.ericpinto.posts.domain.model.Post;
+import com.ericpinto.posts.domain.model.User;
 import com.ericpinto.posts.domain.services.PostService;
+import com.ericpinto.posts.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostController {
 
     @Autowired
-    private PostService userService;
+    private PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> findAll(){
+        List<Post> list = postService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
 
     @GetMapping(value ="/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id){
-        Post post = userService.findById(id);
+        Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
     }
 
--- src/main/java/com/ericpinto/posts/domain/services/PostService.java
+++ src/main/java/com/ericpinto/posts/domain/services/PostService.java
@@ -1,16 +1,23 @@
 package com.ericpinto.posts.domain.services;
 
 import com.ericpinto.posts.domain.model.Post;
+import com.ericpinto.posts.domain.model.User;
 import com.ericpinto.posts.domain.services.exception.ObjectNotFoundException;
 import com.ericpinto.posts.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
+
+    public List<Post> findAll() {
+        return postRepository.findAll();
+    }
 
     public Post findById(String id) {
         return postRepository.findById(id).orElseThrow(()-> new ObjectNotFoundException("Publicação não encontrada"));
